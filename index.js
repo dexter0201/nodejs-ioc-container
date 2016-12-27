d9fe60e2c6c8c78d6652a9d65d51b8f763015ee6
Dexter Nguyen <lich.nguyen@amblique.com>
2016-12-27 16:14:52 +0700

    Supported singleton & update readme

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 'use strict';
 
 var Container = require('./container');
+var containerInstance = null;
+
+function getContainer() {
+    if (containerInstance === null) {
+        containerInstance = new Container();
+    }
+    
+    return containerInstance;
+}
 
 module.exports = {
-    Container: Container
+    getContainer: getContainer
 };
